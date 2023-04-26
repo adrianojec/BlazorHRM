7354021c6d9f75cbe305cd7df622e760de31053e
adrianojec <adrianojeric.28@gmail.com>
2023-04-26 22:09:13 +0800

    Added employee details

--- BlazorHRM/Components/EmployeeCard.razor.cs
+++ BlazorHRM/Components/EmployeeCard.razor.cs
@@ -8,8 +8,18 @@ namespace BlazorHRM.Components
   {
     [Parameter]
     public Employee Employee { get; set; } = default!;
+
     [Parameter]
     public EventCallback<Employee> OnClickQuickView { get; set; }
+
+    [Inject]
+    public NavigationManager NavigationManager { get; set; } = default!;
+
+    public void NavigateToEmployeeDetails(int employeeId)
+    {
+      NavigationManager.NavigateTo($"/employee-overview/{employeeId}");
+    }
+
   }
 }
 
---
+++ BlazorHRM/Pages/EmployeeDetails.razor.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using BlazorHRM.Models;
+using BlazorHRM.Shared.Domain;
+using Microsoft.AspNetCore.Components;
+
+namespace BlazorHRM.Pages
+{
+  public partial class EmployeeDetails
+  {
+    [Parameter]
+    public String EmployeeId { get; set; }
+
+    public Employee? Employee { get; set; }
+
+    protected override Task OnInitializedAsync()
+    {
+      Employee = MockDataService.Employees.FirstOrDefault(employee => employee.EmployeeId == int.Parse(EmployeeId));
+
+      return base.OnInitializedAsync();
+    }
+  }
+}
+
